02222b735e924c8a26ad6b2f8854e1a58f7622d9
Gledyson Fernandes Maia <gledysonmaia09@gmail.com>
2025-02-18 14:33:21 -0300

    fim do desafio

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Eu vou sofrer da maldição</h1>
       <h2>Emmet funcionou</h2>
+      <p>Caixa de texto aleatória: <input type="text" name="" id="itxt"/></p>
     </div>
   );
 }
