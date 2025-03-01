43c3fa3fdc1bf6c320720bf6656d3d9d96716db8
Gledyson Fernandes Maia <gledysonmaia09@gmail.com>
2025-03-01 09:16:47 -0300

    alteração

--- 5_FORMULARIOS_REACT/form/src/components/MyForm.js
+++ 5_FORMULARIOS_REACT/form/src/components/MyForm.js
@@ -1,18 +1,31 @@
 import "./MyForm.css";
+import { useState } from "react";
 
 const MyForm = () => {
+  // 3 - gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => setName(e.target.value);
+
+  console.log(name)
+  
   return (
     <div>
       {/* 1 - criação de forms */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite o seu nome" />
+          <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
         </div>
         {/* 2 - label envolvendo input */}
         <label>
           <span>E-mail:</span>
-          <input type="email" name="email" placeholder="Digite o seu e-mail" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+          />
         </label>
         <input type="submit" value="Enviar" />
       </form>
