65f91003306754032e05c913320cd9f10585691a
Gledyson Fernandes Maia <gledysonmaia09@gmail.com>
2025-03-17 15:33:51 -0300

    atualização antes da próxima aula

---
+++ 6_SECRET_WORD/secretword/src/components/Game.js
@@ -0,0 +1,12 @@
+import "./Game.css";
+
+const GameOver = ({ verifyLetter }) => {
+  return (
+    <div>
+      <h1>Game</h1>
+      <button onClick={verifyLetter}>Finalizar jogo</button>
+    </div>
+  );
+};
+
+export default GameOver;
