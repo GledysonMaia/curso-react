6493bd50b5a2b7b0d23fc307ae3315bb0a2fd0d7
Gledyson Fernandes Maia <gledysonmaia09@gmail.com>
2025-03-19 17:53:31 -0300

    att

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -22,26 +22,63 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  // pick a random category
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category);
+
+    // pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return { word, category };
+  };
+
   // starts the secret word game
   const startGame = () => {
-    setGameStage(stages[1].name)
-  }
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
+    setGameStage(stages[1].name);
+  };
 
   // process the letter input
   const verifyLetter = () => {
-    setGameStage(stages[2].name)
-  }
+    setGameStage(stages[2].name);
+  };
 
   // restarts the game
   const retry = () => {
-    setGameStage(stages[0].name)
-  }
- 
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
       {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
-      {gameStage === "end" && <GameOver retry={retry}/>}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
