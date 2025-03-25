e906a5120ed571d506cb2b931c469696f1810f54
Gledyson Fernandes Maia <gledysonmaia09@gmail.com>
2025-03-25 10:21:52 -0300

    concluiu seção

--- 8_REACT_ROUTER/routerreact/src/components/Navbar.js
+++ 8_REACT_ROUTER/routerreact/src/components/Navbar.js
@@ -1,14 +1,26 @@
 // 2 - links com react router
-import "./Navbar.css"
-import { Link } from "react-router-dom"
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">Sobre</Link>
+      {/*<Link to="/">Home</Link>
+        <Link to="/about">Sobre</Link>*/}
+      <NavLink
+        to="/"
+        // className={({ isActive }) => (isActive ? "esta-ativo" : "esta-inativo")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        // className={({ isActive }) => (isActive ? "esta-ativo" : "esta-inativo")}
+      >
+        Sobre
+      </NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
