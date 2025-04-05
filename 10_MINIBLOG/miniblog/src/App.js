1300387e116bd0efd6b1894c90dbbb768e8ca3f0
Gledyson Fernandes Maia <gledysonmaia09@gmail.com>
2025-04-05 17:36:44 -0300

    terminei de estudar por hoje

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // pages
 import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 // components
 import Navbar from "./components/Navbar/Navbar";
@@ -14,11 +16,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         <Footer />
